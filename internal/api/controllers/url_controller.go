0a935129850ca049661bdbfa9e9853225ab9a884
Igor Bonfim <igormeirabonfim@gmail.com>
2025-02-07 00:34:46 -0300

    Feat: implements cors policy

--- internal/api/controllers/url_controller.go
+++ internal/api/controllers/url_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
+	"net/url"
 
 	"github.com/igormbonfim/shorturl-api/internal/usecases"
 )
@@ -24,6 +25,17 @@ func (u *UrlController) CreateUrl(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil || request.URL == "" {
 		http.Error(w, "Invalid Request", http.StatusBadRequest)
+		return
+	}
+
+	urlValid := isValidURL(request.URL)
+	if !urlValid {
+		response := map[string]string{
+			"message": "Invalid URL",
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(response)
 		return
 	}
 
@@ -50,4 +62,9 @@ func (u *UrlController) GetUrl(w http.ResponseWriter, r *http.Request) {
 	}
 
 	http.Redirect(w, r, url.OriginalUrl, http.StatusFound)
+}
+
+func isValidURL(rawURL string) bool {
+	parsedURL, err := url.ParseRequestURI(rawURL)
+	return err == nil && parsedURL.Scheme != "" && parsedURL.Host != ""
 }
--- internal/api/routes/routes.go
+++ internal/api/routes/routes.go
@@ -7,7 +7,7 @@ import (
 )
 
 func RegisterRoutes() {
-	http.HandleFunc("/api/url", handlePost)
+	http.HandleFunc("/api/url", corsMiddleware(handlePost))
 	http.HandleFunc("/", handleGet)
 }
 
@@ -26,4 +26,27 @@ func handleGet(w http.ResponseWriter, r *http.Request) {
 	}
 
 	services.UrlController.GetUrl(w, r)
+}
+
+func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		allowedOrigins := []string{"https://www.goshort.tech", "http://localhost:3000"}
+		origin := r.Header.Get("Origin")
+
+		for _, o := range allowedOrigins {
+			if origin == o {
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+			}
+		}
+
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next(w, r)
+	}
 }
